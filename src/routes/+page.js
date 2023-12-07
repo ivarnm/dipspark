def1fb2b497248b17a50917352e9b835d696aa2f
Ivar Myrstad <inm@dips.no>
2023-12-07 15:09:13 +0100

    Get bookings by user

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,7 +2,8 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-  const res = await fetch(`https://dipspark-service.azurewebsites.net/Bookings`);
+  const userId = 1;
+  const res = await fetch(`https://dipspark-service.azurewebsites.net/BookingsByUser?userid=${userId}`);
   const bookings = await res.json();
   console.log(bookings);
   return { bookings };
