ae2cffb4df85d5346875434e91d20d939b80f869
Ivar Myrstad <inm@dips.no>
2024-09-23 17:28:31 +0200

    Change statistics to client loaded and reduce its server calls

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
+  import { css } from '@emotion/css';
   import { theme } from "$lib/stores/stores"; 
 	import GhostLoader from "./halloween/GhostLoader.svelte";
+
+  export let style: any = {};
+
+  $: loader = css`
+    width: ${style.size ? style.size : '14px'};
+    padding: ${style.thickness ? style.thickness : '4px'};
+    background: var(${style.color ? style.color : '--neutral-0'});
+	`;
 
 </script>
 
 {#if $theme === "halloween"}
   <GhostLoader />
 {:else}
-  <div class="loader"></div>
+  <div class="loader {loader}"></div>
 {/if}
 
 <style>
   .loader {
-    width: 14px;
-    padding: 4px;
     aspect-ratio: 1;
     border-radius: 50%;
-    background: var(--neutral-0);
     --_m: 
       conic-gradient(#0000 10%,#000),
       linear-gradient(#000 0 0) content-box;
--- src/routes/admin/statistics/+page.svelte
+++ src/routes/admin/statistics/+page.svelte
@@ -1,34 +1,71 @@
 <script lang="ts">
 	import ParkingSpotUtilization from '$lib/components/statistics/ParkingSpotUtilization.svelte';
 	import Table from '$lib/components/statistics/Table.svelte';
+	import { onMount } from 'svelte';
+	import { GetThemeStatistics, GetAllBookings } from '$lib/Api';
+	import Statistics from '$lib/helpers/Statistics';
+	import { users } from '$lib/stores/stores';
+	import type { BookingStatistic, ParkingSpotStatistic, ThemeStatistic } from '$lib/model/models.js';
+	import Loader from '$lib/components/Loader.svelte';
+	
+	let isLoading = true;
+	let error: string;
+	let statistics: Statistics;
+	let themeStatistics: ThemeStatistic[];
+	let parkingSpotStatistics: ParkingSpotStatistic[];
+	let weeklyBookingStatsistics: BookingStatistic[];
+	let allTimeBookingStatistics: BookingStatistic[];
+	
 
-	export let data;
+	onMount(async () => {
+		try {
+			const bookings = await GetAllBookings(fetch);
+			themeStatistics = await GetThemeStatistics(fetch);
+			statistics = new Statistics(bookings, $users);
+			parkingSpotStatistics = statistics.getParkingSpotStatistics();
+			allTimeBookingStatistics = statistics.getBookingStatistics();
+			weeklyBookingStatsistics = statistics.getWeeklyBookingStatistics();
+		}
+		catch (ex) {
+			console.error(ex);
+			error = "Noe gikk galt under henting av data";
+		}
+		isLoading = false;
+	});
 </script>
 
-<section>
-  <ParkingSpotUtilization parkingSpotUtilization={data.parkingSpotUtilizationStatistics ?? []} />
-
-	<Table
-		title="Bookinger siste 7 dager:"
-		headers={['Bruker', 'Antall bookinger']}
-		columns={['userName', 'bookings']}
-		rows={data.weeklyBookingStatsistics ?? []}
-	/>
-
-	<Table
-		title="Alle bookinger (siden august 2024):"
-		headers={['Bruker', 'Antall bookinger']}
-		columns={['userName', 'bookings']}
-		rows={data.allTimeBookingStatistics ?? []}
-	/>
-
-	<Table
-		title="Temaer:"
-		headers={['Tema', 'Antall brukere']}
-		columns={['label', 'users']}
-		rows={data.themeStatistics ?? []}
-	/>
-</section>
+{#if isLoading}
+	<div class="loader">
+		<Loader style={{color: '--neutral-70', size: '30px', thickness: '5px'}} />
+	</div>
+{:else if error}
+	<p>{error}</p>
+{:else}
+	<section>
+		<ParkingSpotUtilization parkingSpotUtilization={parkingSpotStatistics} />
+		
+		<Table
+			title="Bookinger siste 7 dager:"
+			headers={['Bruker', 'Antall bookinger']}
+			columns={['userName', 'bookings']}
+			rows={weeklyBookingStatsistics}
+		/>
+		
+		<Table
+			title="Alle bookinger (siden august 2024):"
+			headers={['Bruker', 'Antall bookinger']}
+			columns={['userName', 'bookings']}
+			rows={allTimeBookingStatistics}
+		/>
+		
+		<Table
+			title="Temaer:"
+			headers={['Tema', 'Antall brukere']}
+			columns={['label', 'users']}
+			rows={themeStatistics}
+		/>
+	</section>
+{/if}
 
 <style>
 	section {
@@ -38,4 +75,15 @@
 		gap: 3rem;
 		padding-bottom: 5rem;
 	}
+
+	.loader {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	p {
+		color: var(--error);
+	}
+
 </style>
