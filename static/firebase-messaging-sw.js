999e8aa466adc38454c4733085b896c93ba31f88
Ivar Myrstad <inm@dips.no>
2025-02-05 14:39:39 +0100

    enable sw click event, close and repoen app

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,56 +1,53 @@
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCSkB8k_k8VfEIp-huw_QPhrXcVWjeE-U4",
-  authDomain: "dipspark-2fa19.firebaseapp.com",
-  projectId: "dipspark-2fa19",
-  storageBucket: "dipspark-2fa19.firebasestorage.app",
-  messagingSenderId: "884190036925",
-  appId: "1:884190036925:web:6dcbc6fe41f1283fbdc8d4",
-  measurementId: "G-GZ22LZZ3J0"
+	apiKey: 'AIzaSyCSkB8k_k8VfEIp-huw_QPhrXcVWjeE-U4',
+	authDomain: 'dipspark-2fa19.firebaseapp.com',
+	projectId: 'dipspark-2fa19',
+	storageBucket: 'dipspark-2fa19.firebasestorage.app',
+	messagingSenderId: '884190036925',
+	appId: '1:884190036925:web:6dcbc6fe41f1283fbdc8d4',
+	measurementId: 'G-GZ22LZZ3J0'
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("Received background message: ", payload);
-  console.log(JSON.stringify(payload));
-  if (!payload.data) return;
-  
-  self.registration.showNotification("Hello from service worker", {
-    body: payload.data?.body,
-    icon: "/logo_maskable.png",
-    data: {
-      url: 'https://dipspark-git-push-ivarnms-projects.vercel.app/book'
-    }
-  });
+	console.log('Received background message: ', payload);
+	console.log(JSON.stringify(payload));
+	if (!payload.data) return;
+
+	self.registration.showNotification('Hello from service worker', {
+		body: payload.data?.body,
+		icon: '/logo_maskable.png',
+		data: {
+			url: 'https://dipspark-git-push-ivarnms-projects.vercel.app/book'
+		}
+	});
 });
 
-// self.addEventListener("notificationclick", (event) => {
-//   console.log("Notification clicked:", event.notification);
-//   event.notification.close();
+self.addEventListener('notificationclick', (event) => {
+	console.log('Notification clicked:', event.notification);
+	event.notification.close();
 
-//   const urlToOpen = event.notification.data?.url || "https://dipspark-git-push-ivarnms-projects.vercel.app/book";
+	const urlToOpen =
+		event.notification.data?.url || 'https://dipspark-git-push-ivarnms-projects.vercel.app/book';
 
-//   event.waitUntil(
-//     clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
-//       const matchingClient = clientList.find(client =>
-//         client.url.startsWith("https://dipspark-git-push-ivarnms-projects.vercel.app")
-//       );
+	event.waitUntil(
+		clients.matchAll({ type: 'window', includeUncontrolled: true }).then(async (clientList) => {
+			const matchingClient = clientList.find((client) =>
+				client.url.startsWith('https://dipspark-git-push-ivarnms-projects.vercel.app')
+			);
 
-//       if (matchingClient) {
-//         // If the app is already open, focus it and navigate to the correct page
-//         return matchingClient.focus().then(() => {
-//           return matchingClient.navigate(urlToOpen);
-//         });
-//       } else {
-//         // Otherwise, open the app in a new tab
-//         return clients.openWindow(urlToOpen);
-//       }
-//     })
-//   );
-// });
+			if (matchingClient) {
+				// If the app is already open, focus it and navigate to the correct page
+				await matchingClient.close();
+			}
+			return clients.openWindow(urlToOpen);
+		})
+	);
+});
 
 // interface NotificationOptions {
 //   badge?: string;
