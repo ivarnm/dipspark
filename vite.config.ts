3dcd0da38d0ebba49aafb4609afffe07f267ef73
Ivar Myrstad <inm@dips.no>
2025-09-04 14:47:15 +0200

    Fix running in https locally

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default defineConfig(({ command }) => ({
+	plugins: [
+		sveltekit(),
+		...(command === 'serve' ? [basicSsl()] : [])
+	],
+	server: {
+    proxy: {}
+  }
+}));
