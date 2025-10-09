dd9be12701724b5008958f59e28ce216d99a9d6e
Frank Hope <frho@dips.no>
2025-10-09 14:32:32 +0200

    Update information page to reflect APPCOA

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,31 +1,9 @@
-<script lang="ts">
-  import {onMount} from "svelte";
-
-  let parkIdLink = '';
-
-  function detectPlatform() {
-    const userAgent = navigator.userAgent || (window as any).opera;
-
-    if (/android/i.test(userAgent)) {
-      parkIdLink = 'https://play.google.com/store/apps/details?id=com.profectum.parkidmobile';
-    } else if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
-      parkIdLink = 'https://apps.apple.com/no/app/parkid/id1146754106';
-    } else {
-      parkIdLink = 'https://leieparkering.no/MobileApp/Default.aspx';
-    }
-}
-
-  onMount(() => {
-    detectPlatform();
-  });
-</script>
-
 <section>
   <p>
     DIPS Park er en intern side for ansatte ved DIPS Trondheim for reservasjon av parkeringsplasser.
   </p>
   <p>
-    Kontoret disponerer flere parkeringsplasser i B-området (U1) i Solsiden Parkeringshus. 
+    Kontoret disponerer 6 parkeringsplasser i B-området (U1) i Solsiden Parkeringshus. 
     Én av parkeringsplassene er fast tildelt en av våre ansatte. Når fast bruker ikke bruker plassen kan øvrige ansatte ved kontoret bruke den.
   </p>
 
@@ -34,31 +12,27 @@
     For å benytte seg av gratis parkering må du først knyttes til DIPS sin parkeringsavtale:
   </p>
   <ol>
-    <li>Send din epost til kontorleder</li>
+    <li>Send ditt telefonnummer til kontorleder</li>
     <li>Kontorleder sørger for å opprette bruker og knytte den til avtale</li>
-    <li>Kontorleder oppretter bruker for deg på siden her</li>
-    <li>Du mottar en mail når dette er utført</li>
-    <li>Last ned appen <a href="{parkIdLink}" target="_blank">ParkID</a> og registrer deg og bilen din</li>
-    <li>TIPS: Valider at du blir belastet med 0,- når du begynner å parkere</li>
+    <li>Du mottar en SMS når dette er utført</li>
+    <li>Last ned appen APPCOA FLOW og registrer deg og bilen din. Viktig at det er samme telefonnummer som du har gitt til kontorleder.</li>
+    <li>Parkeringsrabatten aktiveres ved å godkjenne avtalen. Meny → Fordelsparkering → Velg fordelen → Ferdigstill registrering</li>
+    <li>TIPS: Valider at du blir belastet med 0,- første gang du parkerer</li>
   </ol>
 
   <h2>Hvordan fungerer det?</h2>
   <p>
-    Biler som kjører inn i P-huset på Solsiden blir automatisk registrert med kameragjenkjenning i systemet easyPay. 
-    Er du registrert i internsystemet ParkID sjekkes ditt registreringsnummer mot registrerte data i systemet og 
+    Biler som kjører inn i P-huset på Solsiden blir automatisk registrert med kameragjenkjenning.
+    Er du registrert i APPCOA sjekkes ditt registreringsnummer mot registrerte data i systemet og 
     får opp at du har en gyldig avtale. Det har ikke noe å si hvor på B-området du parkerer. 
     Parkerer du på et annet område blir du belastet som en "vanlig kunde" og opptar ikke en DIPS-plass.
   </p>
   <p>
-    Du kan dermed parkere uten å foreta deg noe mer. Unntaket fra dette er når alle plassene DIPS disponerer allerede er i bruk. 
-    Du vil da få et varsel i ParkID-appen at det ikke er noen ledige plasser igjen og du må aktivere en egen parkering som "vanlig kunde". 
+    Merk at bil nummer 7 (eller mer) vil måtte betale for parkeringen.
   </p>
   <p>
-    Merk at appen ParkID kun brukes til å gi varsel dersom alle plassene er opptatt.
-    Dersom du parkerer selv om plassene er opptatt må du huske å betale for parkeringen.
-    Dette gjøres enten via easyPay sin app, automat, <a href="https://easypay.profectum.no/">easypay.profectum.no</a> (innen 48 timer etter avreise) 
-    eller kjør ut og motta faktura i posten. 
-    Systemet fungerer slik at det er de første som kommer til plassen som vil bli sluppet inn i avtalen. 
+    Det er ingen kobling mellom DIPS Park og APPCOA. Så det er viktig at man registrerer seg på DIPS Park før man parkerer på B-området.
+    Les mer om APPCOA FLOW og fordelsparkering <a href="https://leieparkering.no/MobileApp/Default.aspx">her</a>.
   </p>
 </section>
 
