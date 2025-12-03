e0d312945e842d7b323352ddfdf9208fa3a266fe
Ivar Myrstad <inm@dips.no>
2025-12-03 10:29:36 +0100

    Enable christmas

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,18 +1,18 @@
-export const TOTAL_PARKING_SPOTS = 9
+export const TOTAL_PARKING_SPOTS = 9;
 
-export const MAX_BOOKING_DAYS_FORWARD = 5
-export const MAX_CANCELLATION_DAYS_FORWARD = 9
+export const MAX_BOOKING_DAYS_FORWARD = 5;
+export const MAX_CANCELLATION_DAYS_FORWARD = 9;
 
-export const DATE_FORMAT = 'yyyy-MM-dd'
-export const DATE_OFFSET = 0
+export const DATE_FORMAT = 'yyyy-MM-dd';
+export const DATE_OFFSET = 0;
 
 export const THEMES = [
-  { value: 'system', label: 'Automatisk' },
-  { value: 'light', label: 'Lys' },
-  { value: 'dark', label: 'Mørk' },
-  { value: 'warm', label: 'Varm' },
-  { value: 'cool', label: 'Kald' },
-  // { value: 'halloween', label: 'Halloween' },
-  // { value: 'christmas', label: 'Jul' },
-  // { value: 'winter', label: 'Vinter' },
-]; 
+	{ value: 'system', label: 'Automatisk' },
+	{ value: 'light', label: 'Lys' },
+	{ value: 'dark', label: 'Mørk' },
+	{ value: 'warm', label: 'Varm' },
+	{ value: 'cool', label: 'Kald' },
+	// { value: 'halloween', label: 'Halloween' },
+	{ value: 'christmas', label: 'Jul' }
+	// { value: 'winter', label: 'Vinter' },
+];
